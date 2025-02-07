cbb81609172999b8b9e96a28e60bc770a935c8dc
maserra95 <marcosserra95@hotmail.com>
2025-02-06 21:21:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 function cambiarPagina(pagina) {
     window.location.href = pagina;
 }
+
+function redirectToSpotify(artistId) {
+    // Intentar abrir la app de Spotify (si está instalada)
+    window.location = "spotify://artist/" + artistId;
+
+    // Si la app no está instalada, abrir la página web en una nueva pestaña
+    setTimeout(function() {
+        window.open("https://open.spotify.com/artist/" + artistId, "_blank");
+    }, 25);
+}
+
+function redirectToSpotify2(albumId) {
+    // Intentar abrir la app de Spotify (si está instalada)
+    window.location = "spotify://album/" + albumId;
+
+    // Si la app no está instalada, abrir la página web en una nueva pestaña
+    setTimeout(function() {
+        window.open("https://open.spotify.com/album/" + albumId, "_blank");
+    }, 25);
+}
